d4c03a671d6debaf4a4b5f83cb55228a2c9fcf5e
unknown <wilsonkaroki@gmail.com>
2021-11-28 13:02:49 +0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const routes = require('./routes/routes');
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    name: 'session',
+    secret: 'my_secret is mine and mine alone',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 3600 * 1000, // 1hr
+    }
+}));
+
+//running static files 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.send('Internal Server Error');
+});
+
+const port = process.env.PORT | 8000;
+app.listen(port, ()=>console.log(`Server starting at port ${port}`));
